71a248cc0dc648ceea1b2ff6ee826038e36d49bb
vanchung1995 <vanchung1995@gmail.com>
2024-12-06 00:06:48 +0700

    update structure project

---
+++ simple-elasticsearch-text/src/main/java/com/fpt/chungvv3/simple_crud_spring/SimpleCrudSpringApplication.java
@@ -0,0 +1,13 @@
+package com.fpt.chungvv3.simple_crud_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudSpringApplication.class, args);
+	}
+
+}
---
+++ simple-elasticsearch-text/src/test/java/com/fpt/chungvv3/simple_crud_spring/SimpleCrudSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fpt.chungvv3.simple_crud_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
